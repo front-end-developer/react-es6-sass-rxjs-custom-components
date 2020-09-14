ca5108a4846b47a8e73a1af5e309af6f09fdbb70
Mark Webley <markw@intellisense.io>
2020-09-14 11:37:31 +0100

    tweaks and fine tunning still needed

--- src/components/common/carousel/Carousel.js
+++ src/components/common/carousel/Carousel.js
@@ -5,8 +5,28 @@ import React, { useState, useEffect, useRef } from 'react';
 import './carousel.styles.scss';
 
 const Carousel = ({clickHandler, items}) => {
-    const [currentItem, setCurrentItem] = useState({item: 0});
-    const componentContext = useRef(null);
+    const [currentItem, setCurrentItem] = useState({item: 1});
+    const componentRef = useRef();
+
+    const setMiddleIndex = () => {
+        const domItem = componentRef.current;
+        const carouselWidth= parseInt(window.getComputedStyle(domItem).width);
+        if (items.length) {
+            const carouselItemWidth = parseInt(window.getComputedStyle(domItem.querySelector('.carousel-item')).width);
+            const itemsPerSet = Math.floor(carouselWidth / carouselItemWidth) ;
+            setCurrentItem({
+                item: Math.floor((items.length - itemsPerSet) / 2) + 1
+            });
+        }
+    }
+
+    useEffect(() => {
+        setMiddleIndex();
+    }, [items]);
+
+    /**
+     * @description     could have used componentRef ref here, but this is a core javascript example
+     */
     const moveItemLeft = (event) => {
         if (currentItem.item <= -1) {
             return;
@@ -22,11 +42,21 @@ const Carousel = ({clickHandler, items}) => {
         });
     }
 
+    /**
+     * @description     could have used componentRef ref here, but this is a core javascript example
+     */
     function moveItemRight(event) {
-        if (currentItem.item >= items.length -3) {
+        const domItem = event.target.parentElement;
+        const carouselWidth= parseInt(window.getComputedStyle(domItem).width);
+        const carouselItemWidth = parseInt(window.getComputedStyle(event.target.parentElement.querySelector('.carousel-item')).width);
+        const itemsPerSet = Math.floor(carouselWidth / carouselItemWidth);
+        const lastRowSet = Math.ceil((items.length+1) / itemsPerSet);
+        // TODO:  (items.length - itemsPerSet) // items.length not working as expected because flexbox causings carousel items to be placed in the middle
+
+
+        if (currentItem.item >=  items.length -3) {
             return;
         }
-        const domItem = event.target.parentElement;
         setCurrentItem({item: ++currentItem.item});
         items.map(element => {
             const margins = 52;
@@ -57,7 +87,7 @@ const Carousel = ({clickHandler, items}) => {
 
     return (
         <>
-            <div className="mw-carousel" ref={componentContext}>
+            <div className="mw-carousel" ref={componentRef}>
                 <div className="btn-left" onClick={moveItemLeft}> &lt; </div>
                 <div className="carousel-content">{ createElements }</div>
                 <div className="btn-right" onClick={moveItemRight}> &gt; </div>
